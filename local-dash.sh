51e38d49b3062741d96ef0314da6e6386169c3e8
doristeo <tengizi@gmail.com>
2021-02-25 11:00:09 +0300

    Etherscan links

--- local-dash.sh
+++ local-dash.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-# ver.0.5
+# ver.0.5.2
 export COLOR_NC='\e[0m' # No Color
 export COLOR_GREEN='\e[0;32m'
 export COLOR_RED='\e[0;31m'
+bold=$(tput bold)
+normal=$(tput sgr0)
+echo ""
 echo -n "Bee version " ; bee version
 echo ""
 if pgrep -x "bee" > /dev/null
 then
-    echo "Your address:"
-    curl -s localhost:1635/addresses | jq .ethereum
-    echo "Your chequebook:"
-    curl -s localhost:1635/chequebook/address | jq .chequebookaddress
+    na=$( curl -s localhost:1635/addresses | jq .ethereum | tr -d '"')
+    cba=$(curl -s localhost:1635/chequebook/address | jq .chequebookaddress | tr -d '"')
+    echo "Your address: ${bold}$na${normal}"
+    echo "  Etherscan https://goerli.etherscan.io/address/$na"
+    echo "Your chequebook: ${bold}$cba${normal}"
+    echo "  Etherscan https://goerli.etherscan.io/address/$cba"
     echo ""
     echo -e "Bee node is ${COLOR_GREEN}running :)${COLOR_NC}"
     systemctl status bee | grep 'Active:'
@@ -29,6 +34,7 @@ else
       exit 0
     fi
 fi
+
 echo ""
 PS3='Please enter your choice:'
 options=("Balance" "Manual cashout" "chequebook --gBzz--> node" "chequebook <--gBzz-- node" "Restart node" "Quit")
